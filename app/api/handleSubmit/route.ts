54688bbe0cbd95c277d55a210a953de9dff450ae
Declan Wade <declan.wade@outlook.com.au>
2024-02-24 15:34:44 +0800

    Initial commiit

---
+++ app/api/handleSubmit/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import { Octokit } from "octokit";
+
+export async function POST(request: Request){
+    try {
+    const octokit = new Octokit({
+        auth: 'YOUR-TOKEN'
+      })
+    await octokit.request('POST /repos/declan-wade/roster-master/issues', {
+    owner: 'declan-wade',
+    repo: 'roster-master',
+    title: 'Found a bug',
+    body: request.body,
+    assignees: [
+        'declan-wade'
+    ],
+    labels: [
+        'bug'
+    ],
+    headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+    }).then((response)=> {
+        return NextResponse.json(response);
+    })
+
+    
+  } catch {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,115 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import type { PutBlobResult } from "@vercel/blob";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [formValid, setFormValid] = useState(true);
+  const [nameValid, setNameValid] = useState(true);
+  const [blobUrl, setBlobUrl] = React.useState("")
+  const [filename, setFilename] = React.useState("");
+  const inputFileRef = React.useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = React.useState<PutBlobResult | null>(null);
+
+  const updateFilename = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    const file = e.target.files[0];
+    const { name } = file;
+    setFilename(name);
+  }
+
+  const handleUpload = async (e: any) => {
+    if (!inputFileRef.current?.files) {
+      throw new Error("No file selected");
+    }
+    const file = inputFileRef.current.files[0];
+    const response = await fetch(
+      `/api/handleUpload?filename=${file.name}`,
+      {
+        method: "POST",
+        body: file,
+      }
+    );
+
+    const newBlob = (await response.json()) as PutBlobResult;
+
+    setBlob(newBlob);
+    setBlobUrl(newBlob.url)
+  }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (name.length > 1) {
+      const response = await fetch(
+        `/api/handleUpload`,
+        {
+          method: "POST",
+          body: JSON.stringify({"name": name, "description": description}),
+        }
+      );
+        console.log(response)
+    } else {
+      setNameValid(false);
+    }
+  };
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <div>
+      <Navbar expand="lg" bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/avatar.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Raise an Issue</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <div className="container mt-5">
+        <h4>👋 Start Here</h4>
+        <br></br>
+        <Form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <Form.Label className="form-label">Name or Company</Form.Label>
+            <Form.Control
+              type="text"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
-          </a>
-        </div>
+          </div>
+          <div className="mb-3">
+            <Form.Label className="form-label">Description of issue</Form.Label>
+            <Form.Control
+              as="textarea" rows={3}
+              className="form-control"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Form.Label>Screenshots or supporting images</Form.Label>
+            <InputGroup className="mb-3">
+              <Form.Control type="file" multiple ref={inputFileRef} onChange={updateFilename} />
+              <Button onClick={handleUpload} disabled={filename.length <= 0}>Upload</Button>
+            </InputGroup>
+          </div>
+          <br></br>
+          <Button type="submit">Submit</Button>
+        </Form>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
