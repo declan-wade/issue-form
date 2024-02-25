dcb70e1c545c5559cd55f5908f7582495d3fbe3f
Declan Wade <declan.wade@outlook.com.au>
2024-02-25 13:28:44 +0800

    Removed PAT

--- app/api/handleSubmit/route.ts
+++ app/api/handleSubmit/route.ts
@@ -6,7 +6,7 @@ export async function POST(request: Request){
     const incoming = await request.json()
     console.info(await incoming)
     const octokit = new Octokit({
-        auth: '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'
+        auth: process.env.OCTOKIT_TOKEN
       })
     const response = await octokit.request('POST /repos/declan-wade/roster-master/issues', {
     owner: 'declan-wade',
